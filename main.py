89b2f201e137d45e2b4c0923476859fa6e82f509
NastiaDamian <nastuhta2005@gmail.com>
2023-06-02 19:37:41 +0300

    heirdom

---
+++ main.py
@@ -0,0 +1,65 @@
+class Vehicle:
+    def __init__(self, make, model, year, weight):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.weight = weight
+
+    def start_engine(self):
+        print("The vehicle's engine is starting...")
+
+
+class Car(Vehicle):
+    def __init__(self, make, model, year, weight, num_doors, num_passengers):
+        super().__init__(make, model, year, weight)
+        self.num_doors = num_doors
+        self.num_passengers = num_passengers
+
+    def start_engine(self):
+        print("The car's engine is starting...")
+
+    def drive(self):
+        print("The car is being driven.")
+
+
+class Truck(Vehicle):
+    def __init__(self, make, model, year, weight, cargo_capacity, towing_capacity):
+        super().__init__(make, model, year, weight)
+        self.cargo_capacity = cargo_capacity
+        self.towing_capacity = towing_capacity
+
+    def start_engine(self):
+        print("The truck's engine is starting...")
+
+    def haul(self):
+        print("The truck is hauling.")
+
+
+class Motorcycle(Vehicle):
+    def __init__(self, make, model, year, weight, num_wheels, has_sidecar):
+        super().__init__(make, model, year, weight)
+        self.num_wheels = num_wheels
+        self.has_sidecar = has_sidecar
+
+    def start_engine(self):
+        print("The motorcycle's engine is starting...")
+
+    def ride(self):
+        print("The motorcycle is being ridden.")
+
+
+
+
+car = Car("Honda", "Civic", 2020, 1200, 4, 5)
+truck = Truck("Ford", "F-150", 2022, 5000, 5000, 10000)
+motorcycle = Motorcycle("Harley-Davidson", "Sportster", 2022, 300, 2, False)
+
+car.start_engine()
+car.drive()
+
+truck.start_engine()
+truck.haul()
+
+motorcycle.start_engine()
+motorcycle.ride()
+
